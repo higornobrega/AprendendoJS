e3e6d4c06fc2d4d72dfebd3d2adf7bf70b0fef3a
Higor Nóbrega <higorst.nobrega@gmail.com>
2020-03-24 21:20:52 -0300

    Controle de fluxo [if e else]

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,3 +1,7 @@
 //console.log('O console funcionou corretamente')
-var vel = 60.5
+var vel = 100.5
 console.log(`A velocidade de seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cindo de segurança!`)
