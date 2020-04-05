e3ca3792e7bc51fba5743030bed2eb96265af3ca
Higor Nóbrega <higorst.nobrega@gmail.com>
2020-04-05 15:01:45 -0300

    Repetições (Parte 1) - Curso JavaScript #13

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1;
+do{
+    console.log(`Passe ${c}`);
+    c++;
+}while (c <= 600);
+
+/*
+var c = 1;
+while (c <= 6) {
+    console.log(`Passe ${c}`);
+    c++;
+}
+
+*/
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
