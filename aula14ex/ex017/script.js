a7d72d9962023d5a49b8c9287392026416bbf50b
Higor Nóbrega <higorst.nobrega@gmail.com>
2020-04-09 16:52:04 -0300

    Exercícios JavaScript (Parte 6)

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,18 @@
-window.alert('Olá!')
+function gerarTabuada() {
+    let numero = document.getElementById('numero');
+    let tab = document.getElementById('seltab');
+
+    if (numero.value.length == 0) {
+        alert('Por favor digite um número');
+    }else{
+        let num = Number(numero.value);
+        tab.innerHTML = '';
+        for (let index = 1; index <= 10; index++) {
+           let item = document.createElement('option');
+           item.text = `${num} x ${index} = ${num*index}`
+           item.value = `tab${index}`;
+           tab.appendChild(item)
+        }
+    }
+
+}
