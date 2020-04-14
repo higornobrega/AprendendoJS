8ac673d192d0a01edf3fb936d7c5a83da5d3ddcf
Higor Nóbrega <higorst.nobrega@gmail.com>
2020-04-14 12:28:43 -0300

    Exercícios JavaScript (Parte 8) - Curso JavaScript

--- aula16ex/ex18/script.js
+++ aula16ex/ex18/script.js
@@ -1,6 +1,7 @@
-const vet = [];
-const res = document.getElementById('res');
-const nume = document.getElementById('num');
+let valores = [];
+let res = document.getElementById('res');
+let num = document.getElementById('num');
+let lista = document.getElementById('tab');
 
 function isNumero(n) {
     if (Number(n) >= 1 && Number(n) <= 100) {
@@ -18,15 +19,48 @@ function inLista(n, l) {
 }
 
 function adicionar () {
-    if (isNumero(nume.value) && !inLista(nume.value, vet)) {
-        alert('Tudo ok')
-       
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value));
+        let item = document.createElement('option');
+        item.text = `Valor ${num.value} adicionado`;
+        lista.appendChild(item);
+        res.innerHTML = '';
     } else {       
         alert('Número inválido ou não digitado')
     }
+    num.value = '';
+    num.focus();
     
 }
 
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalizar');
+    } else {
+        let tot = valores.length;
+        let maior = valores[0];
+        let menor = valores[0];
+        let soma = 0;
+        let mendia = 0;
+        for (let pos in valores) {
+            soma += valores[pos];
+            if (valores[pos] > maior) {
+                maior = valores[pos];
+            }
+            if (valores[pos] < menor) {
+                menor = valores[pos];
+            }
+        }
+        media = soma/tot;
+        res.innerHTML = '';
+        res.innerHTML += `<p> Ao todo, temos ${tot} números cadastrados.</p>`;
+        res.innerHTML += `<p> O maior valor informado foi ${maior}.</p>`;
+        res.innerHTML += `<p> O menor valor informado foi ${menor}.</p>`;
+        res.innerHTML += `<p> A média dos valores digitados é ${media}.</p>`;
+
+    }
+}
 /*
 //Minha solução
 
